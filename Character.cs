42e28a43b08f8c398cb823ac4a77a0a0e6dc4f2d
carlo-baron <carlobaron430@gmail.com>
2025-06-12 21:39:31 +0800

    Added .csproj

--- Character.cs
+++ Character.cs
@@ -1,7 +1,7 @@
 public class Character{
-    IAttacker attacker;
-    IMover mover;
-    ISpeaker speaker;
+    private IAttacker attacker;
+    private IMover mover;
+    private ISpeaker speaker;
     public Character(IAttacker attacker, IMover mover, ISpeaker speaker){
         this.attacker = attacker;
         this.mover = mover; 
@@ -19,5 +19,17 @@ public class Character{
     
     public void PerformSpeak(){
         speaker.Speak();
+    }
+
+    public void SetAttackerBehavior(IAttacker attacker){
+        this.attacker = attacker;
+    }
+
+    public void SetMoverBehavior(IMover mover){
+        this.mover = mover; 
+    }
+
+    public void SetSpeakerBehavior(ISpeaker speaker){
+        this.speaker = speaker;
     }
 }
