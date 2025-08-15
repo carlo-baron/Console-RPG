4a8babd5a1ce23e5e0cb13cc212d15ab08c1e97f
carlo-baron <carlobaron430@gmail.com>
2025-08-15 22:16:15 +0800

    Character Factory with default Character maker

---
+++ src/Factories/CharacterFactory.cs
@@ -0,0 +1,9 @@
+namespace RPG.Characters.Factories;
+
+using RPG.Characters;
+
+public static class CharacterFactory{
+    public static Character CreateDefaultCharacter(){
+        return new Character();
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -2,12 +2,15 @@
 
 using RPG.Characters; 
 using RPG.Characters.Components;
+using RPG.Characters.Factories;
 
 public class Program{
     public static void Main(string[] args){
-        Character character = new Character(speaker: new Talk(), attacker: new BasicAttack(40));
-        character.PerformSpeak("Kamusta Kana Bhai");
-        character.PerformAttack();
-        character.PerformMove();
+        //Character character = new Character(speaker: new Talk(), attacker: new BasicAttack(40));
+        //character.PerformSpeak("Kamusta Kana Bhai");
+        //character.PerformAttack();
+        //character.PerformMove();
+        Character defaultCharacter = CharacterFactory.CreateDefaultCharacter(); 
+        defaultCharacter.PerformSpeak("Hello Brody");
     }
 }
